2f2c5050f9bb2cdfcef1a66b91887f01058b9cef
Peter Archman <archman.peter@gmail.com>
2022-02-22 20:48:46 +0100

    Add number

--- main.py
+++ main.py
@@ -3,3 +3,11 @@
 shopping_dict = {
   "piekarnia" : ["chleb", "bułki", "pączek"],
   "warzywniak" : ["marchew", "seler", "rukola"]
+}
+
+for key in shopping_dict:
+  print("Idę do", key, "kupuję następujące rzeczy:", shopping_dict[key])
+
+item_number = len(shopping_dict["piekarnia"]) + len(shopping_dict["warzywniak"])
+
+print("W sumie kupuję", item_number, "produktów.")
